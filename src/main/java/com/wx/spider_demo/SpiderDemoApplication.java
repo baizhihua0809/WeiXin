8a0b70d6c612176f9126f25149d5de94db50034c
xabzh <xabzh@tairanchina.com>
2018-10-12 09:13:56 +0800

    add new weixin project

---
+++ src/main/java/com/wx/spider_demo/SpiderDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wx.spider_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wx/spider_demo/contants/MenuKey.java
@@ -0,0 +1,14 @@
+package com.wx.spider_demo.contants;
+
+/**
+ * Created by Administrator on 2018/10/10.
+ */
+public class MenuKey {
+    public static final String CHANGE_NEWS = "CHANGE_NEWS";
+    public static final String HELP = "HELP";
+    public static final String HOT_SONG = "HOT_SONG";
+    public static final String TOP_500 = "TOP_500";
+    public static final String NET_HOT_SONG = "NET_HOT_SONG";
+    public static final String HUAYU_SONG = "HUAYU_SONG";
+    public static final String XINAO_SONG = "XINAO_SONG";
+}
